efbd3c7c0a1dc742e135b110dbc92ce87ffaba8e
omarjeb <omar.jebali@outlook.com>
2023-05-31 16:23:12 +0100

    React Props Checkpoint

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+import Navigation from './Components/Navigation';
+import TourList from './Components/TourList';
+
+function App() {
+  return (
+    <div className="App">
+    <Navigation/>
+
+    <TourList/>
+    </div>
+  );
+}
+
+export default App;
